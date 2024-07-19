ed639edf7c7b197a6af6defb9b4e5c8f30e87dff
Анастасия Гражданцева <tronshkaktab@gmail.com>
2024-07-19 15:33:19 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+    description: 'Customers мои Customers'
+};
+
+export default function CustomersPage() {
+    return <p>Customers Page</p>
+}
